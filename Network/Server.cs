453dccd683e816911cba605653f8e269749ebd08
Ales Mikholap <ales.mikholap@gmail.com>
2014-04-12 10:59:51 +0300

    Basic events for speech recognition

--- Network/Server.cs
+++ Network/Server.cs
@@ -230,7 +230,14 @@ namespace Network
                     if (base.StartAsyncPing(client.Addr, Network.Properties.Settings.Default.PING_PORT_OUT_SERVER) == false)
                     {
                         logger.Debug("ping remove " + IPAddress.Parse(client.Addr.ToString()));
-                        Dispatcher.Invoke((Action)(() => _clients.Remove(client)));  // remove non-ping client
+                        try
+                        {
+                            Dispatcher.Invoke((Action)(() => _clients.Remove(client)));  // remove non-ping client
+                        }
+                        catch (TaskCanceledException)
+                        {
+                            // main window closed
+                        }
                     }
                 }
                 Thread.Sleep(TimeOut);
@@ -369,9 +376,10 @@ namespace Network
             _receiving = true;
             while (_receiving)
             {
-                while (udpClient.Available == 0)
+                if (udpClient.Available == 0)
                 {
                     Thread.Sleep(100);
+                    continue;
                 }
                 buffer = udpClient.Receive(ref clientEP);
 
@@ -407,6 +415,7 @@ namespace Network
             Thread listenClientsInfoThread = new Thread(this.ListenClientsInfo);
             listenNewClientsThread.Start();
             listenClientsInfoThread.Start();
+            this.TalkerChanged += SpeechRecognizer.Server_TalkerChanged;
         }
 
         /// <summary>
@@ -414,6 +423,8 @@ namespace Network
         /// </summary>
         public override void Stop()
         {
+            this.TalkerChanged -= SpeechRecognizer.Server_TalkerChanged;
+
             _isWorking = false;
             Thread.Sleep(1000);    // let worker threads finish
 
---
+++ Network/SpeechRecognizer.cs
@@ -0,0 +1,47 @@
+﻿using Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network
+{
+    public static class SpeechRecognizer
+    {
+        private static byte[] _buffer;
+
+        public static event EventHandler<EventArgs> SpeechRecognized;
+
+        static SpeechRecognizer()
+        {
+            AudioIO.MergedTick += AudioIO_MergedTick;
+        }
+
+        private static void OnSpeechRecognized(EventArgs e)
+        {
+            if (SpeechRecognized != null)
+            {
+                SpeechRecognized(null, e);
+            }
+        }
+
+        public static void AudioIO_MergedTick(object sender, AudioIOEventArgs e)
+        {
+            if (e.Item == null)
+            {
+                return;
+            }
+
+            Array.Resize(ref _buffer, _buffer.Length + e.Item.Data.Length);
+            e.Item.Data.CopyTo(_buffer, _buffer.Length - e.Item.Data.Length);
+        }
+
+        public static void Server_TalkerChanged(object sender, TalkerChangedEventArgs e)
+        {
+            // send _buffer to Google
+            OnSpeechRecognized(EventArgs.Empty);
+            _buffer = new byte[0];
+        }
+    }
+}
